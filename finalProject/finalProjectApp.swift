a0ff3aff7461f1a49a96c4a491b3953773131936
Sumayya Mariyam <sumayyamariyam2005@gmail.com>
2023-06-26 13:06:36 -0400

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
